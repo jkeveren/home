a066830fe79a98e8e7f9b14549b19160e191f3ef
James Keveren <james@keve.ren>
2020-06-03 19:38:52 +0200

    new light 4

--- devices.mjs
+++ devices.mjs
@@ -6,7 +6,7 @@ export default [
 	['Light 1',        'ESP_A75A6D.localdomain', [light, dflt]],
 	['Light 2',        'ESP_A50EFF.localdomain', [light]],
 	['Light 3',        'ESP_AA3C57.localdomain', [light]],
-	['Light 4',        'ESP_A7A449.localdomain', [light, dflt]],
+	['Light 4',        'ESP_E3A603.localdomain', [light, dflt]],
 	['Kitchen Lights', 'ESP_754970.localdomain', [light, dflt]],
 	['Mirror Lights',  'ESP_9E6DD4.localdomain', [light]],
 	['Extractor',      'ESP_B1213E.localdomain', [appliance]],
