abd7388e096f5a43126f40ca008c4fc6e8ea5ffa
James Keveren <james@keve.ren>
2020-07-24 00:31:23 +0200

    removed .localdomain

--- devices.mjs
+++ devices.mjs
@@ -3,14 +3,14 @@ const appliance = 'appliance';
 const dflt = 'default';
 
 export default [
-	['Light 1',        'ESP_A75A6D.localdomain', [light, dflt]],
-	['Light 2',        'ESP_A50EFF.localdomain', [light]],
-	['Light 3',        'ESP_AA3C57.localdomain', [light]],
-	['Light 4',        'ESP_E3A603.localdomain', [light, dflt]],
-	['Kitchen Lights', 'ESP_754970.localdomain', [light, dflt]],
-	['Mirror Lights',  'ESP_9E6DD4.localdomain', [light]],
-	['Extractor',      'ESP_B1213E.localdomain', [appliance]],
-	['Speakers',       'ESP_B3A241.localdomain', [appliance]],
+	['Light 1',        'ESP_A75A6D', [light, dflt]],
+	['Light 2',        'ESP_A50EFF', [light]],
+	['Light 3',        'ESP_AA3C57', [light]],
+	['Light 4',        'ESP_E3A603', [light, dflt]],
+	['Kitchen Lights', 'ESP_754970', [light, dflt]],
+	['Mirror Lights',  'ESP_9E6DD4', [light]],
+	['Extractor',      'ESP_B1213E', [appliance]],
+	['Speakers',       'ESP_B3A241', [appliance]],
 ].map(a => ({
 	name: a[0],
 	hostname: a[1],
